a10cf0019abe2c9fb7c26032a67cdc9d5af52fac
Fernando <delpino.cafer20@zaragoza.salesianos.edu>
2024-06-16 16:49:54 +0200

    Gameplay funcional: - Se pueden seleccionar las armas de la lista para adivinar - Las armas seleccionadas aparecen en la lista de intentos - En cada intento se pueden ver los fallos, aciertos, aciertos parciales 	y si el número objetivo es mayor o menor en algunos casos - Se detecta cuando el arma correcta es elegida (no hace nada aún) - Se pueden filtrar las armas por su nombre en la lista de selección

--- src/java/controlador/JuegoControlador.java
+++ src/java/controlador/JuegoControlador.java
@@ -26,7 +26,17 @@ public class JuegoControlador implements Serializable{
     private boolean partidaActiva;
     private Arma armaJuego;
     private List<Arma> listaArmas;
+    private List<Arma> listaSeleccArma;
     private List<Arma> listaIntentos;
+    private String campoFiltro;
+    private boolean victoria;
+    
+    
+    private final String CORREC = "celdaCorrecto";
+    private final String INCORREC = "celdaIncorrecto";
+    private final String MAYOR = "celdaMayor";
+    private final String MENOR = "celdaMenor";
+    private final String PARCI = "celdaParcial";
     
     public JuegoControlador() {
     }
@@ -35,7 +45,30 @@ public class JuegoControlador implements Serializable{
     public void init(){
         emf = Persistence.createEntityManagerFactory("GundlePU");
         partidaActiva = false;
+        victoria = false;
+        listaArmas = listadoArmas();
+        listaSeleccArma = new ArrayList(listaArmas);
+    }
+    
+    
+    
+    public void iniciarPartida(){
+        listaArmas = listadoArmas();
+        partidaActiva = true;
+        listaIntentos = new ArrayList();
+        armaJuego = armaAleatoria();
+        victoria = false;
+    }
+    
+    public void adivinar(Arma arma){
+        if(listaIntentos.contains(arma))
+            return;
         
+        listaIntentos.add(arma);
+        if(arma.equals(armaJuego)){
+            victoria = true;
+            System.out.println("************** VICTORIA BITCHES ******************");
+        }
     }
     
     public Arma armaAleatoria(){
@@ -83,6 +116,121 @@ public class JuegoControlador implements Serializable{
         return arma;
     }
     
+    public void filtrarArmas(){
+        EntityManager em = emf.createEntityManager();
+        List<Arma> armas = new ArrayList<>();
+        try{
+            if(campoFiltro != null && !campoFiltro.equals("")){
+                Query q = em.createQuery("SELECT a FROM Arma a WHERE LOWER(a.nombre) LIKE LOWER(:nombre)").setParameter("nombre", "%"+campoFiltro+"%");
+                armas = q.getResultList();
+                listaSeleccArma = armas;
+            }
+            else{
+                listaSeleccArma = listadoArmas();
+            }
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            em.close();
+        }
+    }
+    
+    public String estiloCalidad(Character valor){
+        // compareTo devuelve 0 si el carácter es el mismo
+        if(valor.compareTo(armaJuego.getCalidad()) == 0)
+            return CORREC;
+        else
+            return INCORREC;
+    }
+    
+    public String estiloTipo(TipoArma valor){
+        if(valor.equals(armaJuego.getTipo()))
+            return CORREC;
+        else
+            return INCORREC;
+    }
+    
+    public String estiloClase(ClaseArma valor){
+        if(valor.equals(armaJuego.getClase()))
+            return CORREC;
+        else
+            return INCORREC;
+    }
+    
+    public String estiloCargador(Integer valor){
+        if(valor == null)
+            return (armaJuego.getCargador() == null)? CORREC : INCORREC;
+        
+        // si no tiene cargador devuelve siempre menor, a no ser que se compare con otro arma sin cargador
+        if(armaJuego.getCargador() == null){
+            return MENOR;
+        }
+        
+        if(armaJuego.getCargador().intValue() == valor.intValue())
+            return CORREC;
+        
+        return (armaJuego.getCargador() > valor)? MAYOR : MENOR;
+    }
+    
+    public String estiloMunicion(Integer valor){
+        if(valor == null)
+            return (armaJuego.getMunicion()== null)? CORREC : INCORREC;
+        
+        // si munición infinita devuelve siempre mayor,
+        // a no ser que se compare con otro arma con munición infinita
+        if(armaJuego.getMunicion()== null){
+            return MAYOR;
+        }
+        
+        if(armaJuego.getMunicion().intValue() == valor.intValue())
+            return CORREC;
+        
+        return (armaJuego.getMunicion()> valor)? MAYOR : MENOR;
+    }
+    
+    public String estiloProyectiles(Integer valor){
+        if(valor == null){
+            return (armaJuego.getProyectiles()== null)? CORREC : INCORREC;
+        }  
+        
+        if(armaJuego.getProyectiles()== null){
+            return MENOR;
+        }
+        
+        if(armaJuego.getProyectiles().intValue() == valor.intValue())
+            return CORREC;
+        
+        return (armaJuego.getProyectiles()> valor)? MAYOR : MENOR;
+    }
+    
+    public String estiloColores(List<Color> colores){
+        if(colores == null || colores.isEmpty()){
+            if(armaJuego.getColorList() == null || armaJuego.getColorList().isEmpty())
+                return CORREC;
+            else
+                return INCORREC;
+        }
+        
+        boolean perfecto = true;
+        boolean parcial = false;
+        
+        for(Color c : colores){
+            if(armaJuego.getColorList().contains(c))
+                parcial = true;
+            else
+                perfecto = false;
+        }
+        
+        if(perfecto && colores.size() == armaJuego.getColorList().size())
+            return CORREC;
+        else
+            return (parcial)? PARCI : INCORREC;
+        
+    }
+    
     // ----- GETTER / SETTER -----
 
     public boolean isPartidaActiva() {
@@ -115,6 +263,30 @@ public class JuegoControlador implements Serializable{
 
     public void setListaIntentos(List<Arma> listaIntentos) {
         this.listaIntentos = listaIntentos;
+    }
+
+    public String getCampoFiltro() {
+        return campoFiltro;
+    }
+
+    public void setCampoFiltro(String campoFiltro) {
+        this.campoFiltro = campoFiltro;
+    }
+
+    public List<Arma> getListaSeleccArma() {
+        return listaSeleccArma;
+    }
+
+    public void setListaSeleccArma(List<Arma> listaSeleccArma) {
+        this.listaSeleccArma = listaSeleccArma;
+    }
+
+    public PrincipalControlador getPrincipalCtrl() {
+        return principalCtrl;
+    }
+
+    public void setPrincipalCtrl(PrincipalControlador principalCtrl) {
+        this.principalCtrl = principalCtrl;
     }
     
     
